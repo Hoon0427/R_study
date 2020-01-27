6c07fc6301b3199a6648be8421758bce063a52e4
Hoon0427 <59523979+Hoon0427@users.noreply.github.com>
2020-01-27 17:32:02 +0900

    Add files via upload

---
+++ r_study_1.R
@@ -0,0 +1,56 @@
+mean(x<-c(1,2,3))
+x
+mean(x=c(1,2,3))
+x
+
+a<-3
+b<-4.5
+c<-a+b
+print(c)
+c
+
+one<-80
+two<-90
+three<-75
+four<-NA
+is.na(four)
+x<-NULL
+is.null(x)
+is.null(1)
+is.null(NA)
+is.na(NULL)
+ 
+is.even <- NULL
+if (a °¡ Â¦¼ö¸é) {
+  is_even <- TRUE
+} else {
+    is_even <- FALSE
+}
+
+TRUE & TRUE
+TRUE&FALSE
+TRUE|TRUE
+TRUE|FALSE
+!TRUE
+!FALSE
+
+T<- FALSE
+TRUE<-FALSE
+
+c(TRUE, TRUE) & c(TRUE, FALSE)
+c(TRUE, TRUE) && c(TRUE, FALSE)
+
+sex <- factor("m", c("m","f"))
+sex
+levels(sex)[1]
+levels(sex)[2]
+
+sex
+levels(sex) <-c("male","female")
+sex
+
+factor(c("m", "m", "f"), c("m", "f"))
+
+factor(c("m","m","f"))
+ordered("a", c("a","b","c"))
+
