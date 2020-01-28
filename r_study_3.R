46b5bb82f78c5c722567b41d36e7e5a03fc5ea79
Hoon0427 <59523979+Hoon0427@users.noreply.github.com>
2020-01-28 21:33:08 +0900

    Add files via upload

---
+++ r_study_3.R
@@ -0,0 +1,14 @@
+(x<-list(name="foo", height=70))
+
+(x<-list(name="foo", height=c(1,3,5)))
+
+list(a=list(val=c(1,2,3)), b=list(val=c(1,2,3,4)))
+
+x<-list(name="foo", height=c(1,3,5))
+x$name
+x$height
+x[[1]]
+x[[2]]
+
+x[1]
+x[2]
