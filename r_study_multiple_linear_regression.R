abc8ed229bed1873134286b91cfaa168cea8653f
Hoon0427 <ikaros977@gmail.com>
2020-02-08 00:22:47 +0900

    multiple_linear_regression

--- r_study_multiple_linear_regression.R
+++ r_study_multiple_linear_regression.R
@@ -41,3 +41,51 @@ abline(2.25, 0.80, lty=1)
 abline(2.25 + 1.45, 0.80, lty=2) 
 abline(2.25 + 1.94, 0.80, lty=3) 
 legend("topright", levels(iris$Species), pch=1:3, bg="white")
+
+
+x<-1:1000
+y<-x^2 + 3 * x + 5 + rnorm(1000) 
+lm(y ~ I(x^2) + x) 
+
+
+lm(y ~ x^2) 
+
+
+x1 <- 1:1000 
+x2 <- 3 * x1 
+y <- 3 * (x1 + x2) + rnorm(1000) 
+lm(y ~ I(x1 + x2)) 
+
+
+
+lm(y ~ x1 + x2) 
+
+x<-101:200
+y<-exp(3 * x + rnorm(100))
+lm(log(y) ~ x)
+
+
+x<-101:200
+y<-log(x) + rnorm(100)
+lm(y ~ log(x))
+
+
+
+data(Orange)
+Orange
+
+with(Orange,
+     plot(Tree, circumference, xlab="tree", ylab="corcumference"))
+
+with(Orange, interaction.plot(age, Tree, circumference))
+
+
+m<-lm(circumference ~ fTree * age, data=Orange)
+anova(m)
+
+
+head(model.matrix(m))
+
+
+mm<-model.matrix(m)
+mm[,grep("age",colnames(mm))]
