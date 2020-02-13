08d6b8f81a7ec6e895578ebfd35c24ac3ed2e453
Hoon0427 <ikaros977@gmail.com>
2020-02-14 00:15:26 +0900

    prime_num_choi

---
+++ prime_num_choi.R
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+frime_func <- function(x) {
+  prime_set <- c()
+  for(j in 2:x) {
+    count <- 0
+    for(i in 1:j) {
+      if(j %% i == 0 ) {
+        count <- count + 1
+      }
+    }
+    # print(count)
+    if(count == 2) {
+      # print(j)
+      prime_set <- append(prime_set, j)
+    }
+  }
+  return(prime_set)
+}
+
+
+system.time(frime_func(1000))
+
+
+prime_set <- c()
+
+
+for(j in 2:1000) {
+  count <- 0
+  for(i in 1:j) {
+    if(j %% i == 0 ) {
+      count <- count + 1
+    }
+  }
+  # print(count)
+  if(count == 2) {
+    # print(j)
+    prime_set <- append(prime_set, j)
+  }
+}
+
+prime_set
+
+
+for(i in 1:7) {
+  if(7 %% i == 0 ) {
+    count <- count + 1
+  }
+}
+
+
+count
