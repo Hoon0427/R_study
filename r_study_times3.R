e635a9af883aaa47bc38bf6dbaeef11495c66725
Hoon0427 <ikaros977@gmail.com>
2020-02-14 22:08:59 +0900

    times_3

--- r_study_times3.R
+++ r_study_times3.R
@@ -1,4 +1,17 @@
+library(append)
+?append
 
+
+a<-c(print(i))
+b<-c(print(j))
+
+
+
+for(j in 1:10){
+  if (j%%5==0){
+    print(j)
+  }
+}
 
 for(i in 1:10){
   if(i%%3==0){
@@ -6,3 +19,12 @@ for(i in 1:10){
   }
 }
 
+data.frame(print(i),print(j))
+
+for(i in 1:10){
+  if(i%%3==0){
+    print(i)
+    a<-print(i)
+    a<-append(c(a,i))
+  }
+}
