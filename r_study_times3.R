2da60d515eae042c80832b757c82dc0937617490
Hoon0427 <ikaros977@gmail.com>
2020-02-25 12:56:25 +0900

    200225_graph

--- r_study_times3.R
+++ r_study_times3.R
@@ -6,18 +6,49 @@ a<-c(print(i))
 b<-c(print(j))
 
 
+a <- c()
 
-for(j in 1:10){
-  if (j%%5==0){
-    print(j)
+for(j in 1:1000){
+  if(j == 1000) {
+    break
+  }
+    if (j%%5==0 | j%%3==0){
+    a <- append(a, j)
   }
 }
 
-for(i in 1:10){
+a
+
+sum(a)
+
+
+
+500 / 0
+
+
+
+
+
+b<-append(2,3)
+append(b,5)
+
+for(i in 1:100){
   if(i%%3==0){
     print(i)
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 data.frame(print(i),print(j))
 
---
+++ study_graph.R
@@ -0,0 +1,30 @@
+str(iris)
+plot(iris)
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width")
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",main="iris")
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch="20")
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch="+")
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=.5)
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, col="blue")
+
+min(iris$Sepal.Length)
+max(iris$Sepal.Length)
+min(iris$Sepal.Width)
+max(iris$Sepal.Width)
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5))
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5),type="l")
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5),type="b")
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5),type="o")
+
+data(cars)
+str(cars)
+plot(cars)
+
+plot(cars,type="l")
