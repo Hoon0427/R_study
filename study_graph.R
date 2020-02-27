2a391faa802726cbe406989cb484cae98cefd935
Hoon0427 <ikaros977@gmail.com>
2020-02-27 21:42:37 +0900

    experiment

--- study_graph.R
+++ study_graph.R
@@ -18,7 +18,17 @@ max(iris$Sepal.Length)
 min(iris$Sepal.Width)
 max(iris$Sepal.Width)
 
-plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5))
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2,
+     xlim=c(4,8.5),ylim=c(1.5,5))
+
+
+plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, 
+     xlim=c(min(iris$Sepal.Length),max(iris$Sepal.Length)),ylim=c(min(iris$Sepal.Width),max(iris$Sepal.Width)))
+
+
+
+
 plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5),type="l")
 plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5),type="b")
 plot(iris$Sepal.Length, iris$Sepal.Width,xlab="Sepal.Length",ylab="Sepal.width",pch=20,cex=2, xlim=c(4,8.5),ylim=c(1.5,5),type="o")
