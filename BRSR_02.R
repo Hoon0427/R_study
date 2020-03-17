842e71359fe1fb976816033ea4fbb81e33ed1c71
Hoon0427 <ikaros977@gmail.com>
2020-03-17 22:47:30 +0900

    study

--- BRSR_02.R
+++ BRSR_02.R
@@ -21,3 +21,38 @@ object.size(MovieLense)
 object.size(as(MovieLense, "matrix"))
 
 object.size(as(MovieLense, "matrix")) / object.size(MovieLense)
+
+similarity_users <- similarity(MovieLense[1:4,], method = "cosine",
+                               which = "users")
+
+class(similarity_users)
+
+as.matrix(similarity_users)
+
+image(as.matrix(similarity_users), main = "User similarity")
+
+similarity_items <- similarity(MovieLense[, 1:4],
+                               method = "cosine", which = "items")
+as.matrix(similarity_items)
+
+image(as.matrix(similarity_items), main = "Item similarity")
+
+recommender_models <- recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+
+names(recommender_models)
+
+lapply(recommender_models, "[[", "description")
+
+recommender_models$IBCF_realRatingMatrix$parameters
+
+library(recommenderlab)
+library(ggplot2)
+data("MovieLense")
+class(MovieLense)
+
+dim(MovieLense)
+
+slotNames(MovieLense)
+
+class(MovieLense@data)
+dim(MovieLense)
