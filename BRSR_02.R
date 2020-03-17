8df04c66b8d7e8556121360614065dc9a35e2bf8
Hoon0427 <ikaros977@gmail.com>
2020-03-17 21:43:19 +0900

    study

--- BRSR_02.R
+++ BRSR_02.R
@@ -12,3 +12,12 @@ data_package$results[,"Item"]
 
 data("MovieLense")
 MovieLense
+
+class(MovieLense)
+
+methods(class = class(MovieLense))
+
+object.size(MovieLense)
+object.size(as(MovieLense, "matrix"))
+
+object.size(as(MovieLense, "matrix")) / object.size(MovieLense)
