ff79e06dbdecf2b0add0ca803f3430eacbd8ed72
Hoon0427 <ikaros977@gmail.com>
2020-03-23 12:47:08 +0900

    study

--- BRSR_02.R
+++ BRSR_02.R
@@ -262,3 +262,28 @@ qplot(rowSums(ratings_movies_watched)) +
   geom_vline(xintercept = mean(rowSums(ratings_movies_watched)),
              col = "red", linetype = "dashed") +
   ggtitle("Distribution of movies by user")
+
+ratings_movies_watched <- binarize(ratings_movies, minRating = 1)
+
+qplot(rowSums(ratings_movies_watched)) +
+  stat_bin(binwidth = 10) +
+  geom_vline(xintercept = mean(rowSums(ratings_movies_watched)), col = "red", linetype = "dashed") +
+  ggtitle("Distribution of movies by user")
+                                 
+which_train <- sample(x = c(TRUE, FALSE), size = nrow(ratings_movies),
+                      replace = TRUE, prob = c(0.8, 0.2)) 
+
+recc_data_train <- ratings_movies[which_train, ]
+recc_data_test <- ratings_movies[!which_train, ]
+
+recc_model <- Recommender(data = recc_data_train,
+                          method = "IBCF", parameter = list(method = "Jaccard"))
+model_details
+
+n_recommended <- 6
+recc_predicted <- predict(object = recc_model, newdata = recc_data_test,
+                          n = n_recommended)
+recc_matrix <- sapply(recc_predicted@items, function(x){colnames(ratings_movies)[x]})
+
+recc_matrix[, 1:4]
+
