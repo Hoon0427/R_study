1d5f9fc4038577f439a20f67d2960c400c7a2a7f
Hoon0427 <ikaros977@gmail.com>
2020-04-01 11:09:44 +0900

    study

--- BRSR_04.R
+++ BRSR_04.R
@@ -74,5 +74,85 @@ head(table_items)
 
 table_items[, category := "product"]
 
-name_countries <- c(countrycode_data$country.name.en, "Taiwan", "UK", "Russia", "Venezuela",
+name_countries <- c(codelist$country.name, "Taiwan", "UK", "Russia", "Venezuela",
                     "Slovenija", "Caribbean", "Netherlands (Holland)", "Europe", "Central America", "MS North Africa")
+table_items[description %in% name_countries, category := "region"]
+
+table_items[grepl("Region", description), category := "region"]
+head(table_items)
+
+table_items[, list(n_items = .N), by = category]
+
+which_train <- sample(x = c(TRUE, FALSE),
+                      size = nrow(ratings_matrix),
+                      replace = TRUE,
+                      prob = c(0.8, 0.2))
+recc_data_train <- ratings_matrix[which_train, ]
+recc_data_test <- ratings_matrix[!which_train, ]
+
+recc_model <- Recommender(data = recc_data_train,
+                          method = "IBCF",
+                          parameter = list(method = "Jaccard"))
+
+class(recc_model@model$sim)
+dim(recc_model@model$sim)
+
+image(recc_model@model$sim)
+
+range(recc_model@model$sim)
+
+dist_rating <- as(recc_model@model$sim, "matrix")
+
+dist_category <- table_items[, 1 - dist(category == "product")]
+class(dist_category)
+
+dist_category <- as(dist_category, "matrix")
+
+dim(dist_category)
+dim(dist_rating)
+
+rownames(dist_category) <- table_items[, id]
+colnames(dist_category) <- table_items[, id]
+
+vector_items <- rownames(dist_rating)
+dist_category <- dist_category[vector_items, vector_items]
+
+identical(dim(dist_category), dim(dist_rating))
+identical(rownames(dist_category), rownames(dist_rating))
+identical(colnames(dist_category), colnames(dist_rating))
+
+image(dist_category)
+
+weight_category <- 0.25
+dist_tot <- dist_category * weight_category + dist_rating * (1 - weight_category)
+
+image(dist_tot)
+
+recc_model@model$sim <- as(dist_tot, "dgCMatrix")
+
+n_recommended <- 10
+recc_predicted <- predict(object = recc_model,
+                          newdata = recc_data_test,
+                          n = n_recommended)
+
+head(recc_predicted@itemLabels)
+
+table_labels <- data.frame(id = recc_predicted@itemLabels)
+table_labels <- merge(table_labels, table_items,
+                      by = "id", all.x = TRUE, all.y = FALSE,
+                      sort = FALSE)
+descriptions <- as(table_labels$description, "character")
+
+head(table_labels)
+
+recc_user_1 <- recc_predicted@items[[1]]
+items_user_1 <- descriptions[recc_user_1]
+head(items_user_1)
+
+recc_matrix <- sapply(recc_predicted@items, function(x){
+  recommended <- descriptions[x]
+  c(recommended, rep("", n_recommended - length(recommended)))
+})
+dim(recc_matrix)
+
+head(recc_matrix[, 1:3])
